c9d6250623827b5b4a6675d389cb7889ca2214d7
moon <moon>
2016-04-30 09:52:14 +0800

    first version

--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -8,19 +8,42 @@ namespace BowlingGameKata
 {
     public class Game
     {
+        public struct set
+        {
+            public int first;
+            public int second;
+            public int third;
+            public bool strike;
+            public set(int fir, int sec, int thir, bool s)
+            {
+                first = fir;
+                second = sec;
+                third = thir;
+                strike = s;
+            }
+        };
 
-        public static int GetScore(List<int> pins)
+        public static int GetScore(List<set> pins)
         {
             int score = 0;
-            for (int i = 0; i < pins.Count;i++ )
+     /*       for (int i = 0; i < pins.Count ;i++ )
             {
-                if (pins[i] == 10)
-                    score = pins[i] + pins[i + 2] + pins[i + 3];
+                if (pins[i] == 10 && pins[i+2] != 10 )
+                    score += pins[i] + pins[i + 2] + pins[i + 3];
+                else if( pins[i] == 10 ) // 連續兩球strike
+                    score += pins[i] + pins[i + 2] + pins[i + 4];
                 else
                     score += pins[i];
             }
+            */
 
-            return score;
+            for (int i = 0; i < pins.Count; i++)
+            {
+                score += pins[i].first + pins[i].second;    
+            }
+
+
+                return score;
         }
     }
 }
--- BowlingGameKata/TestBowling.cs
+++ BowlingGameKata/TestBowling.cs
@@ -10,14 +10,13 @@ namespace BowlingGameKata
     [TestFixture]
     public class TestBowling
     {
+
         [Test]
         public void AllZero()
         {
-            List<int> pins = new List<int> 
-            {
-                0,0,  0,0,  0,0,  0,0,  0,0,
-                0,0,  0,0,  0,0,  0,0,  0,0
-            };
+            List<Game.set> pins = new List<Game.set>();
+            for (int i = 0; i < 10; i++ )
+               pins.Add(new Game.set(0, 0, 0, false));
 
             Assert.That(Game.GetScore(pins), Is.EqualTo(0));
         }
@@ -25,28 +24,48 @@ namespace BowlingGameKata
         [Test]
         public void AllOnes()
         {
-            List<int> pins = new List<int> 
-            {
-                1,1,  1,1,  1,1,  1,1,  1,1, 
-                1,1,  1,1,  1,1,  1,1,  1,1 
-            };
+            List<Game.set> pins = new List<Game.set>();
+            for (int i = 0; i < 10; i++ )
+                pins.Add(new Game.set(1, 1, 0, false));
 
             Assert.That(Game.GetScore(pins), Is.EqualTo((1+1)*10));
         }
-
+/*
         [Test]
         public void OneStrike()
         {
-            List<int> pins = new List<int> 
-            {
-                10,0,  1,1,  1,1,  1,1,  1,1, 
-                1,1,   1,1,  1,1,  1,1,  1,1 
-            };
-
+            List<Game.set> pins = new List<Game.set>();
+            pins.Add(new Game.set(10, 0, 0, true));
+            for (int i = 0; i < 9; i++)
+                pins.Add(new Game.set(1, 1, 0, false));
             Assert.That(Game.GetScore(pins), Is.EqualTo(
                 (10 + 1 + 1) + (1 + 1)* 9));
         }
 
+        [Test]
+        public void TwoStrike()
+        {
+            List<int> pins = new List<int> 
+            {
+                10,0,  10,0,  1,1,  1,1,  1,1, 
+                1,1,   1,1,  1,1,  1,1,  1,1,0 
+            };
 
+            Assert.That(Game.GetScore(pins), Is.EqualTo(
+                (10+10+1)+(10 + 1 + 1)*1 + (1 + 1)*8));
+        }*/
+
+    /*    [Test]
+        public void AllStrike()
+        {
+            List<int> pins = new List<int> 
+            {
+                10,0,  10,0,  10,0,  10,0,  10,0, 
+                10,0,  10,0,  10,0,  10,0,  10,10,10 
+            };
+
+            Assert.That(Game.GetScore(pins), Is.EqualTo(
+             300);
+        }*/
     }
 }
